888c5b805f77447a38817f29fa565329b7c0e231
MartinZatko <54416929+MartinZatko@users.noreply.github.com>
2021-06-24 11:53:24 +0200

    Update mailsend.php

--- mailsend.php
+++ mailsend.php
@@ -1,27 +1,17 @@
 <?php
-$name = $_POST['name'];
-$user_mail = $_POST['email'];
-$message = $_POST['message'];
-$projekt = $_POST['projekt'];
 
-
-$mailfrom = $user_mail;
-
-$email_subject = 'Feedback na web CV';
-
-$email_body = "Meno odosielatela: $name/n".
-                "Email odosielatela: $user_mail/n".
-                    "Projekt: $projekt/n".
-                        "Správa: $message/n";
-
-$to = "martinzatkoiz@gmail.com";
-
-$headers = "From: $mailfrom/r/n";
-
-$headers .= "Reply-to: $user_mail/r/n";
-
-mail($to, $email_subject, $email_body, $headers);
-
-header("Location: index.html");
-
+if(isset($_POST['submit'])) {
+    
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+    
+    $mailTo = 'martinzatkoiz@gmail.com';
+    $headers = "From: ".$mailFrom;
+    $txt = "Máš novú správu od ".$name."./n/n".$message;
+    
+    $mail = mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
 ?>
